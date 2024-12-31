cbd7a7537a3efae49dff52d50b48e7da7b2845ef
Sageer Ahmed <s.hunzai100@gmail.com>
2024-12-31 04:58:16 -0800

    Button Adding

---
+++ DOM/new/script.js
@@ -0,0 +1,7 @@
+function addButton() {
+    let container = document.querySelector('.container');
+    let newButton = document.createElement('button');
+    newButton.textContent = 'New Button';
+    newButton.onclick = addButton;
+    container.appendChild(newButton);
+}
