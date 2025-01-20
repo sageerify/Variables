df134ade4c32624ac4b1ee12f7a940834a9bea41
Sageer Ahmed <160208153+sageerify@users.noreply.github.com>
2025-01-20 00:29:41 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-console.log("ali");
-console.log("is");
-console.log("My best Friend", "muzamil")
+//var
+//let
+//const
+// Variable in JavaScript
+
+var age = 23;
+//Never start from number on age place
+//Reserve words
+//Numbers
+console.log(age);
+
+var x;
+var x =4;
+console.log(x, typeof x)
+//multiple varible can declare in var using same name. 
+
+//Let
+let x = 10;
+console.log(x);
+
+//Const
+const x = 10;
+console.log(x);
+x = 23;
+console.log(x);
+
