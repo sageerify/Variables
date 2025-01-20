15f33d98c2511ac97924398894cafe6f8232e057
Sageer Ahmed <160208153+sageerify@users.noreply.github.com>
2025-01-20 00:28:38 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-let age = 18;
-let message = age >= 18 ? "You are an adult." : "You are a minor.";
-console.log(message);
-
-/*let age = 18;
-let message;
-
-if (age >= 18) {
-  message = "You are an adult.";
-} else {
-  message = "You are a minor.";
-}
-console.log(message);*/
+console.log("ali");
+console.log("is");
+console.log("My best Friend", "muzamil")
